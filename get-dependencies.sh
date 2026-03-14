a6ffac9f7889ad283a4b036179d8b4273ddaf6cf
Link4Electronics <link4electronics@gmail.com>
2026-03-14 01:39:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+	dbus-glib \
     gtk3
 
 echo "Installing debloated packages..."
