1ce34fb990beea30ccc0668e93e9f1d693e3d94f
Link4Electronics <link4electronics@gmail.com>
2026-05-12 18:14:47 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,19 +21,20 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/bin
+echo "Getting Phantom Satellite binary..."
+echo "---------------------------------------------------------------"
 if [ "$ARCH" = "x86_64" ]; then
-  echo "Getting Phantom Satellite binary..."
-  echo "---------------------------------------------------------------"
   TARBALL_LINK=$(wget --retry-connrefused --tries=30 \
 	  https://api.github.com/repos/DCFUKSURMOM/Phantom-Satellite/releases/latest -O - \
-	  | sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*linux-x86_64-gtk3.*tar.xz')
-  echo "$TARBALL_LINK" | awk -F'/' '{print $(NF-1)}' | tr -d 'v' > ~/version
-  wget "$TARBALL_LINK" -O /tmp/phantomsatellite.tar.xz
-  tar xvf /tmp/phantomsatellite.tar.xz -C /usr/lib
-  rm -f /tmp/phantomsatellite.tar.xz
+	  | sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*linux-${ARCH}-gtk3.*tar.xz')
 else
-	make-aur-package gtk2
-	sed -i -e 's|-O3|-O2|' /etc/makepkg.conf
-	make-aur-package
+  TARBALL_LINK=$(wget --retry-connrefused --tries=30 \
+	  https://api.github.com/repos/DCFUKSURMOM/Phantom-Satellite/releases/latest -O - \
+	  | sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*linux-${ARCH}-gtk2.*tar.xz')
+  make-aur-package gtk2
+  make-aur-package libdbusmenu-gtk2
 fi
+echo "$TARBALL_LINK" | awk -F'/' '{print $(NF-1)}' | tr -d 'v' > ~/version
+wget "$TARBALL_LINK" -O /tmp/phantomsatellite.tar.xz
+tar xvf /tmp/phantomsatellite.tar.xz -C /usr/lib
 mv -v /usr/lib/phantomsatellite/* ./AppDir/bin
